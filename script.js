6588bc5f5fb6082021908e17d11aa24753ff516d
Pedro Cherysse <132665796+Cheryssejean@users.noreply.github.com>
2024-12-13 08:37:20 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('greetButton').addEventListener('click', () => {
+    const message = document.getElementById('message');
+    message.textContent = 'You clicked the button! Welcome to JavaScript!';
+  });
+  
